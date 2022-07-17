0ba2311ea5f3df63cec07930faed2af87ee26277
Mohit Raj <rajmohit.129@gmail.com>
2022-07-18 03:54:22 +0530

    [Mohit]Added:to check HashMapLinklist

---
+++ test/MyHashMapTest.java
@@ -0,0 +1,50 @@
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+public class MyHashMapTest {
+    @Test
+
+    public void provideSentence_WhenWordAddedToList_ShouldReturnFrequency()
+    {
+        String sentence = "to be or not to be";
+        MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+        String [] words = sentence.toLowerCase().split(" ");
+        for (String word : words){
+            Integer value = myHashMap.get(word);
+            if(value == null) {
+                value = 1;
+            }
+            else {
+                value = value + 1;
+            }
+            myHashMap.add(word, value);
+        }
+        int frequency = myHashMap.get("to");
+        Assert.assertEquals(2, frequency);
+    }
+
+    @Test
+    public void provideLargeSentence_WhenWordAddedToHasMap_ShouldReturnParanoidFrequency()
+    {
+        String sentence = "paranoid are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately into paranoid " +
+                "avoidable situations";
+        MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+        String [] words = sentence.toLowerCase().split(" ");
+        for (String word : words){
+            Integer value = myHashMap.get(word);
+            if(value == null) {
+                value = 1;
+            }
+            else {
+                value = value + 1;
+            }
+            myHashMap.add(word, value);
+        }
+        int frequency = myHashMap.get("paranoid");
+        Assert.assertEquals(4, frequency);
+    }
+}
